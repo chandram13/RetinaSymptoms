d2e1d6c06de245cba5ea12c69fd3abacc5ccc869
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-18 13:25:26 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+
+namespace retinaIssues{
+    class humanRetina{
+        public static void specificProblems{
+            Retinal_Disease_Conditions = ["Retinal tear","Retinal detachment","Diabetic retinopathy","Epiretinal membrane","Macular hole","Macular Degeneration","Retinitis pigementosa"]
+            majorSymptoms = ["Seeking specks or cobwebs","Blurred vision","Defects in side vision","Lost vision either temporarily or completely"]
+
+        }
+    }
+}
