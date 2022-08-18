83b7c77e5bdba8f7a04452b7b1a6d2159dfacb18
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-18 13:53:55 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,10 +1,46 @@
 
 namespace retinaIssues{
     class humanRetina{
-        public static void specificProblems{
-            Retinal_Disease_Conditions = ["Retinal tear","Retinal detachment","Diabetic retinopathy","Epiretinal membrane","Macular hole","Macular Degeneration","Retinitis pigementosa"]
+        public static void generalSymptoms{
             majorSymptoms = ["Seeking specks or cobwebs","Blurred vision","Defects in side vision","Lost vision either temporarily or completely"]
 
+        }
+        public static void retinalTear{
+            atRisk = ["Being nearsighted","Having glaucoma","Having a severe eye injury","Condition being present in the genetics","Thin spots present in the retina"]
+            symptoms = ["Sudden blurry vision","Increased floaters","Sudden decreases in vision","Signs of headaches"]
+            occurances = ["Seeing flashing lights","Seeing too many floaters","Shadow apperance in vision called side vision.","Appearance of a grey curtain"]
+            treatments = ["Laser surgery (photocoagulation)","Freezing treatment (cryopexy)"]
+            associatedConditions = ["Diabetes","Sickle cell disease","scar tissue","nearsightedness","former eye trauma","history of condition","retinal degeneration","inflammatory disorders","autoimmune diseases","certain cancers"]
+        }
+        public static void retinalDetachment{
+            symptoms = []
+            occurances = []
+            treatments = []
+        }
+        public static void diabeticRetinopathy{
+            symptoms = []
+            occurances = []
+            treatments = []
+        }
+        public static void epiretinalMembrane{
+            symptoms = []
+            occurances = []
+            treatments = []
+        }
+        public static void macularHole{
+            symptoms = []
+            occurances = []
+            treatments = []
+        }
+        public static void macularDegeneration{
+            symptoms = []
+            occurances = []
+            treatments = []
+        }
+        public static void retinitisPigmentosa{
+            symptoms = []
+            occurances = []
+            treatments = []
         }
     }
 }
